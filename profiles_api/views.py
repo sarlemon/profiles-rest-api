9a76c18e7e92a4b3c8c3328b73d06586eb18c5d9
Liu, Mengwei <mengwei.liu@intel.com>
2023-02-11 19:07:40 +0800

    feat: 🎸 Register profile Viewset with the URL router

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 
-from profiles_api import serializers
+from profiles_api import serializers, models
 
 
 # Create your views here.
@@ -85,4 +85,13 @@ class HelloViewSet(viewsets.ViewSet):
 
   def partial_update(self, request, pk=None):
     """Handle updating part of an object"""
+    return Response({ 'http_method': 'PATCH' })
+
+  def destroy(self, request, pk=None):
+    """Handle removing an object"""
     return Response({ 'http_method': 'DELETE' })
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+  """Handle creating and updating profiles"""
+  serializer_class = serializers.UserProfileSerializer
+  queryset = models.UserProfile.objects.all()
