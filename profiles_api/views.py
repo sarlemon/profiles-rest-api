9a79a1279002276a8580ec762400b44e3ce43c31
Liu, Mengwei <mengwei.liu@intel.com>
2023-02-15 22:25:07 +0800

    feat: 🎸 Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers, models
+from profiles_api import serializers, models, permissions
 
 
 # Create your views here.
@@ -95,3 +96,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   """Handle creating and updating profiles"""
   serializer_class = serializers.UserProfileSerializer
   queryset = models.UserProfile.objects.all()
+  authentication_classes = (TokenAuthentication, )
+  permissions_classes = (permissions.UpdateOwnProfile, )
